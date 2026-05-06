8d32a58800e4745be5784e516689d7195b25deaa
Mista299 <michael.tabares@udea.edu.co>
2026-05-06 14:07:46 -0500

    Revert "add backend: pipeline ETL + API REST + modelo DwC"
    This reverts commit 25fc84c4e21671b94b6bd18268487d459351e752.

--- backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.taxon import Taxon
-from models.event import Event
-from models.location import Location
-from models.occurrence import Occurrence
-from models.identification import Identification
-
-__all__ = ["Taxon", "Event", "Location", "Occurrence", "Identification"]
